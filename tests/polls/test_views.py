8b4c5578f391cb8c0f0f1ae6c705ba92015d16e6
Sparrow0hawk <alexjcoleman@gmail.com>
2024-02-24 15:46:27 +0000

    Migrate tests to pytest

--- tests/polls/test_views.py
+++ tests/polls/test_views.py
@@ -1,10 +1,9 @@
-from django.test import TestCase
+from django.test import Client
 from django.urls import reverse
 
 
-class TestPolls(TestCase):
-    def test_index_shows_header(self) -> None:
-        url = reverse("index")
-        response = self.client.get(url)
+def test_index_shows_header(client: Client) -> None:
+    url = reverse("index")
+    response = client.get(url)
 
-        self.assertContains(response, "<h1>Hello world!</h1>")
+    assert b"<h1>Hello world!</h1>" == response.content
