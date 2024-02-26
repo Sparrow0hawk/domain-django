51e867486abced7b80216916b58c1af911fb9a9d
Sparrow0hawk <alexjcoleman@gmail.com>
2024-02-26 20:20:25 +0000

    Set playwright timeout

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -1,3 +1,15 @@
 import os
+from typing import Generator
+
+import pytest
+from playwright.sync_api import BrowserContext
 
 os.environ.setdefault("DJANGO_ALLOW_ASYNC_UNSAFE", "true")
+
+
+@pytest.fixture(name="context")
+def browser_context_fixture(
+    context: BrowserContext,
+) -> Generator[BrowserContext, None, None]:
+    context.set_default_timeout(5_000)
+    yield context
