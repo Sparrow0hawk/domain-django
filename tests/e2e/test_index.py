34bb3c8c54ff0fcbe4d3bd7c09bc7f8e63ee6ce3
Sparrow0hawk <alexjcoleman@gmail.com>
2024-03-23 15:14:44 +0000

    Reorganise e2e tests

--- tests/e2e/test_index.py
+++ tests/e2e/test_index.py
@@ -1,8 +1,8 @@
 from playwright.sync_api import Page
 from pytest_django.live_server_helper import LiveServer
 
-from tests.e2e.client import QuestionRepr, AppClient, ChoiceRepr
-from tests.e2e.pages import PollsPage, QuestionDetailPage
+from tests.e2e.client import QuestionRepr, AppClient
+from tests.e2e.pages import PollsPage
 
 
 def test_index_shows_table(app_client: AppClient, live_server: LiveServer, page: Page) -> None:
@@ -28,22 +28,3 @@ def test_index_shows_poll(app_client: AppClient, live_server: LiveServer, page: 
     question_detail_page = PollsPage.open(page, live_server.url).table["What is the best sandwich?"].open()
 
     assert question_detail_page.question == "What is the best sandwich?"
-
-
-def test_poll_shows_question_and_choices(app_client: AppClient, live_server: LiveServer, page: Page) -> None:
-    app_client.add_questions(
-        QuestionRepr(
-            id=1,
-            question_text="What is the best sandwich?",
-            choices=[
-                ChoiceRepr(id=1, choice_text="Marmite and cheese"),
-                ChoiceRepr(id=2, choice_text="Ham and cheese"),
-                ChoiceRepr(id=3, choice_text="Cheese"),
-            ],
-        )
-    )
-
-    question_detail_page = QuestionDetailPage.open(page, live_server.url, 1)
-
-    assert question_detail_page.question == "What is the best sandwich?"
-    assert question_detail_page.choices() == ["Marmite and cheese", "Ham and cheese", "Cheese"]
---
+++ tests/e2e/test_question_details.py
@@ -0,0 +1,26 @@
+from playwright.sync_api import Page
+from pytest_django.live_server_helper import LiveServer
+
+from tests.e2e.client import AppClient, QuestionRepr, ChoiceRepr
+from tests.e2e.pages import QuestionDetailPage
+
+
+def test_question_details_shows_question_and_choices(
+    app_client: AppClient, live_server: LiveServer, page: Page
+) -> None:
+    app_client.add_questions(
+        QuestionRepr(
+            id=1,
+            question_text="What is the best sandwich?",
+            choices=[
+                ChoiceRepr(id=1, choice_text="Marmite and cheese"),
+                ChoiceRepr(id=2, choice_text="Ham and cheese"),
+                ChoiceRepr(id=3, choice_text="Cheese"),
+            ],
+        )
+    )
+
+    question_detail_page = QuestionDetailPage.open(page, live_server.url, 1)
+
+    assert question_detail_page.question == "What is the best sandwich?"
+    assert question_detail_page.choices() == ["Marmite and cheese", "Ham and cheese", "Cheese"]
